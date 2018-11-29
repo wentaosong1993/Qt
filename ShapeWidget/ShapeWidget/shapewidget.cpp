638829177e5d78af26e596768c5d226d971f5442
songwentao <614425032@qq.com>
2018-11-29 17:12:53 +0800

    不规则窗体设置遮罩
    把图片改为jpg格式可以，png格式不行，另外并没有遮罩的效果，空白的地方依旧为白色，没有遮罩效果。

---
+++ ShapeWidget/ShapeWidget/shapewidget.cpp
@@ -0,0 +1,47 @@
+﻿#include "shapewidget.h"
+#include <QPixmap>
+#include <QBitmap>
+#include <QPainter>
+
+ShapeWidget::ShapeWidget(QWidget *parent)
+    : QDialog(parent)
+{
+    QPixmap pix;
+    pix.load(":/1.png",0,Qt::AvoidDither | Qt::ThresholdDither | Qt::ThresholdAlphaDither);
+    resize(pix.size());
+    setWindowFlags(Qt::FramelessWindowHint);
+    setMask(QBitmap(pix.mask()));
+}
+
+ShapeWidget::~ShapeWidget()
+{
+
+}
+
+void ShapeWidget::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        dragPosition = event->globalPos() - frameGeometry().topLeft();
+        event->accept();
+    }
+    if(event->button() == Qt::RightButton)
+    {
+        close();
+    }
+}
+
+void ShapeWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if(event->buttons() & Qt::LeftButton)
+    {
+        move(event->globalPos() - dragPosition);
+        event->accept();
+    }
+}
+
+void ShapeWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.drawPixmap(0,0,QPixmap(":/1.png"));
+}
---
+++ ShapeWidget/ShapeWidget/shapewidget.h
@@ -0,0 +1,26 @@
+﻿#ifndef SHAPEWIDGET_H
+#define SHAPEWIDGET_H
+
+#include <QDialog>
+#include <QMouseEvent>
+#include <QPoint>
+#include <QPaintEvent>
+
+class ShapeWidget : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ShapeWidget(QWidget *parent = 0);
+    ~ShapeWidget();
+
+protected:
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void paintEvent(QPaintEvent *event);
+
+private:
+    QPoint dragPosition;
+};
+
+#endif // SHAPEWIDGET_H
