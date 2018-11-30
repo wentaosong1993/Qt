ff6999518a0953cecb2f13dd7b609c1dab02b15d
songwentao <614425032@qq.com>
2018-11-30 17:58:42 +0800

    TCP服务器编程--服务器端part

---
+++ TcpServer/TcpServer/tcpserver.cpp
@@ -0,0 +1,22 @@
+﻿#include "tcpserver.h"
+
+TcpServer::TcpServer(QWidget *parent, Qt::WindowFlags f)
+    :QDialog(parent,f)
+{
+    setWindowTitle(QStringLiteral("Tcp Server"));
+    ContentListWidget = new QListWidget;
+    PortLabel = new QLabel(QStringLiteral("端口"));
+    PortLineEdit = new QLineEdit;
+    CreateBtn = new QPushButton(QStringLiteral("创建聊天室"));
+    mainLayout = new QGridLayout(this);
+    mainLayout->addWidget(ContentListWidget,0,0,1,2);
+    mainLayout->addWidget(PortLabel,1,0);
+    mainLayout->addWidget(PortLineEdit,1,1);
+    mainLayout->addWidget(CreateBtn,2,0,1,2);
+}
+
+TcpServer::~TcpServer()
+{
+
+}
+
---
+++ TcpServer/TcpServer/tcpserver.h
@@ -0,0 +1,27 @@
+﻿#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include <QDialog>
+#include <QListWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QGridLayout>
+
+class TcpServer : public QDialog
+{
+    Q_OBJECT
+
+public:
+    TcpServer(QWidget *parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+    ~TcpServer();
+
+private:
+    QListWidget *ContentListWidget;
+    QLabel *PortLabel;
+    QLineEdit *PortLineEdit;
+    QPushButton *CreateBtn;
+    QGridLayout *mainLayout;
+};
+
+#endif // TCPSERVER_H
