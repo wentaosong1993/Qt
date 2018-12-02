3520d89a31cadeda1b77c0c1e116ea6015c65b20
songwentao <614425032@qq.com>
2018-12-02 16:17:41 +0800

    Qt网络应用开发初步
    demo手敲

---
+++ myHTTP/MainWindow/mainwindow.h
@@ -0,0 +1,42 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QFile>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void startRequest(QUrl url);
+
+private slots:
+    void replyFinished(QNetworkReply *reply);
+
+    void httpFinished();
+    void httpReadyRead();
+    void updateDataReadProgress(qint64 bytesRead, qint64 totalBytes);
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    QNetworkAccessManager *manager;
+    QNetworkReply *reply;
+    QUrl url;
+    QFile *file;
+};
+
+#endif // MAINWINDOW_H
