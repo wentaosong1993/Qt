51b78bf270112253b3c71c07f4cf8e70d52fe8aa
songwentao <614425032@qq.com>
2018-12-03 16:40:56 +0800

    多线程简单实例手敲demo

---
+++ Thread/Thread/workthread.cpp
@@ -0,0 +1,19 @@
+﻿#include "workthread.h"
+#include <QDebug>
+
+WorkThread::WorkThread()
+{
+
+}
+
+void WorkThread::run()
+{
+    while(true)
+    {
+        for(int n = 0; n <10; n++)
+        {
+            qDebug() << n << n << n << n << n << n;
+//            printf("%d,%d,%d",n,n,n);//控制台阻塞，程序卡死
+        }
+    }
+}
