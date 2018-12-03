80f3dcba28a32e2d6e097a58ed1625cc12f84b77
songwentao <614425032@qq.com>
2018-12-03 15:07:37 +0800

    Qt5事件处理及实例-手敲demo
    事件过滤及实例

---
+++ EventFilter/EventFilter/eventfilter.h
@@ -0,0 +1,29 @@
+﻿#ifndef EVENTFILTER_H
+#define EVENTFILTER_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QImage>
+#include <QEvent>
+
+class EventFilter : public QDialog
+{
+    Q_OBJECT
+
+public:
+    EventFilter(QWidget *parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+    ~EventFilter();
+
+public slots:
+    bool eventFilter(QObject *watched, QEvent *event);
+private:
+    QLabel *label1;
+    QLabel *label2;
+    QLabel *label3;
+    QLabel *stateLabel;
+    QImage Image1;
+    QImage Image2;
+    QImage Image3;
+};
+
+#endif // EVENTFILTER_H
