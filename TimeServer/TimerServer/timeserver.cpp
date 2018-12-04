bb9828ab6bb4ae81170fc5eb5333a806f11cf3d6
songwentao <614425032@qq.com>
2018-12-04 15:58:46 +0800

    Qt操作QSQlite数据库
    "Unable to fetch row" "插入失败"
    更改setDatabaseName的参数就好了

--- TimeServer/TimerServer/timeserver.cpp
+++ TimeServer/TimerServer/timeserver.cpp
@@ -10,6 +10,7 @@ TimeServer::TimeServer(QObject *parent)
 
 void TimeServer::incomingConnection(int socketDescriptor)
 {
+
     TimeThread *thread = new TimeThread(socketDescriptor,0); //创建线程
     connect(thread,SIGNAL(finished()),dlg,SLOT(slotShow()));
     connect(thread,SIGNAL(finished()),thread,SLOT(deleteLater()),Qt::DirectConnection);
