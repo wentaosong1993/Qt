c8ccffee729e8983d811e0ef8c514c7aa7878e3e
songwentao <614425032@qq.com>
2018-12-05 17:36:09 +0800

    Qt语言家简单测试

---
+++ TestHello/TestHello/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTranslator *translator = new QTranslator;
+    translator->load("D:/GitHubRepository/Qt/TestHello/TestHello/TestHello.qm");
+    a.installTranslator(translator);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
