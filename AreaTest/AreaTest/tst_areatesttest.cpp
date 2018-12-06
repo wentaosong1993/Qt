6b0ccfc94788260e627c526ce5ffe6d47acc540c
songwentao <614425032@qq.com>
2018-12-06 11:22:14 +0800

    Qt5单元测试demo

---
+++ AreaTest/AreaTest/tst_areatesttest.cpp
@@ -0,0 +1,31 @@
+﻿#include <QString>
+#include <QtTest>
+
+#include "area.h"
+
+class AreaTestTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    AreaTestTest();
+
+private Q_SLOTS:
+    void toAreaTest();
+};
+
+AreaTestTest::AreaTestTest()
+{
+}
+
+void AreaTestTest::toAreaTest()
+{
+    Area area(1);
+    QVERIFY(qAbs(area.CountArea() - 3.14) < 0.0000001);
+    QVERIFY2(true, "Failure");
+
+}
+
+QTEST_APPLESS_MAIN(AreaTestTest)
+
+#include "tst_areatesttest.moc"
---
+++ TestQString/TestQString/tst_testqstring.cpp
@@ -0,0 +1,43 @@
+﻿#include <QString>
+#include <QtTest>
+
+class TestQString : public QObject
+{
+    Q_OBJECT
+
+public:
+    TestQString();
+
+private Q_SLOTS:
+    void testToLower();
+    void testToLower_data();
+};
+
+TestQString::TestQString()
+{
+}
+
+void TestQString::testToLower_data()
+{
+    //添加测试列
+    QTest::addColumn<QString>("string");
+    QTest::addColumn<QString>("result");
+
+    //添加测试数据
+    QTest::newRow("lower")<<"hello"<<"hello";
+    QTest::newRow("mixed")<<"heLLo"<<"hello";
+    QTest::newRow("upper")<<"HELLO"<<"hello";
+}
+
+void TestQString::testToLower()
+{
+    QFETCH(QString,string);
+    QFETCH(QString,result);
+
+    QCOMPARE(string.toLower(),result);
+    QVERIFY2(true, "Failure");
+}
+
+QTEST_APPLESS_MAIN(TestQString)
+
+#include "tst_testqstring.moc"
