09282f6001df53db0727863ea86c4c8a6603f2ef
songwentao <614425032@qq.com>
2018-12-06 15:02:34 +0800

    综合实例1part

---
+++ manager/manager/connection.h
@@ -0,0 +1,45 @@
+﻿#ifndef CONNECTION_H
+#define CONNECTION_H
+#include <QtSql>
+#include <QDebug>
+#include "qdom.h"
+
+
+static bool createConnection()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setHostName("easybook-3313b0");
+    db.setDatabaseName("data.db");
+    db.setUserName("songwentao");
+    db.setPassword("123456");
+
+    if(!db.open())
+    {
+        return false;
+    }
+    QSqlQuery query;
+    query.exec(QString("create table factory(id varchar primary key,name varchar)"));
+    query.exec(QString("insert into factory values('0','请选择厂家')"));
+    query.exec(QString("insert into factory values('01','一汽大众')"));
+    query.exec(QString("insert into factory values('02','二汽神龙')"));
+    query.exec(QString("insert into factory values('03','上海大众')"));
+
+    //创建品牌表
+    query.exec(QString("create table brand(id varchar primary key,name varchar, \
+                       factory varchar,price int,sum int,sell int,last int)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+    query.exec(QString("insert into brand vaues('01','奥迪A6','一汽大众',36,50,10,40)"));
+
+    return true;
+}
+
+
+#endif // CONNECTION_H
---
+++ manager/manager/widget.h
@@ -0,0 +1,39 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QMainWindow>
+#include <QMenuBar>
+
+
+namespace Ui {
+class widget;
+}
+
+class Widget : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+    QMenu *manageMenu;
+    QMenu *passwordMenu;
+
+    QAction *manageAction;
+    QAction *chartAction;
+    QAction *quitAction;
+
+private slots:
+    void on_manageMenu_clicked();
+    void on_chartMenu_clicked();
+    void on_quitMenu_clicked();
+
+private:
+    Ui::widget *ui;
+
+    void createMenuBar();
+};
+
+#endif // WIDGET_H
