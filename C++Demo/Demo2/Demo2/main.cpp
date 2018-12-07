d6c6ea8f088e331ba08292ede63ad4e44f236070
songwentao <614425032@qq.com>
2018-12-07 11:39:02 +0800

    二义性问题
    多重继承下的二义性问题

---
+++ C++Demo/Demo2/Demo2/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <QCoreApplication>
+
+class A
+{
+    public:
+    int a;
+};
+class B1 : public A
+{
+    private:
+    int b1;
+};
+class B2 : public A
+{
+private:
+    int b2;
+};
+class C : public B1, public B2
+{
+public:
+    int f();
+private:
+    int c;
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    C c1;
+//    c1.a; //B1、B2中通过继承过来，已经有一个私有成员变量a,该访问在B1\B2\C中无法判别；error
+    c1.A::a;
+    c1.B1::a; //直接访问B1从A中继承过来的a,即直接访问到了B1中的a;
+    c1.B2::a; //同上
+    return 0;
+}
