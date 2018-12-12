d6b719cc083a1caabfaeefd39d8f185ac7a8f7ff
songwentao <614425032@qq.com>
2018-12-12 22:15:21 +0800

    视频demo--UDP通信

---
+++ UDP_demo/UDP/widget.h
@@ -0,0 +1,29 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QUdpSocket>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+    void dealMsg();
+
+private slots:
+    void on_sendBtn_clicked();
+
+private:
+    Ui::Widget *ui;
+    QUdpSocket *udpSocket;
+};
+
+#endif // WIDGET_H
