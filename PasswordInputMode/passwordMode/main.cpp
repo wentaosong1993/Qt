d85509b9b77b062c11cd824123aeb80cb55040a9
songwentao <614425032@qq.com>
2018-12-28 18:25:17 +0800

    Qt自定义控件以及控件的提升
    输入框密码模式

---
+++ PasswordInputMode/passwordMode/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "passwordmode.h"
+#include "passwordinput.h"
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    PasswordMode w;
+//    w.show();
+
+    passwordInput input;
+    input.show();
+
+
+    return a.exec();
+}
---
+++ PasswordInputMode/passwordMode/passwordmode.h
@@ -0,0 +1,17 @@
+﻿#ifndef PASSWORDMODE_H
+#define PASSWORDMODE_H
+
+#include <QWidget>
+#include <QLineEdit>
+
+class PasswordMode : public QLineEdit
+{
+    Q_OBJECT
+
+public:
+    explicit PasswordMode(QWidget *parent = 0);
+    ~PasswordMode();
+
+};
+
+#endif // PASSWORDMODE_H
