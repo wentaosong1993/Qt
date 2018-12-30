1a680e1074a9a06b22faa08f8fb1cd55641f6bad
songwentao <614425032@qq.com>
2018-12-30 14:35:53 +0800

    Qt 中的控件QLineEdit信号editingFinish信号触发没有问题
    验证程序：真正的问题在于这个正则表达式，该源文件中的正则表达式导致在小数点后一位小数时，editingFinish信号无法正常触发。

---
+++ tryout/TryOutOfQLineEdit/main.cpp
@@ -0,0 +1,11 @@
+#include "tryoutofqlineedit.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TryOutOfQLineEdit w;
+    w.show();
+
+    return a.exec();
+}
---
+++ tryout/TryOutOfQLineEdit/tryoutofqlineedit.cpp
@@ -0,0 +1,25 @@
+﻿#include "tryoutofqlineedit.h"
+#include "ui_tryoutofqlineedit.h"
+#include <QDebug>
+
+TryOutOfQLineEdit::TryOutOfQLineEdit(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::TryOutOfQLineEdit)
+{
+    ui->setupUi(this);
+//    QRegExp regxData("^(\\d|10)(\\.\\d\\d)?$");
+//    QValidator *validatorData = new QRegExpValidator(regxData, this);
+//    ui->lineEdit->setValidator(validatorData);
+
+    connect(ui->lineEdit,&QLineEdit::editingFinished,this,&TryOutOfQLineEdit::PrintOutput);
+}
+
+TryOutOfQLineEdit::~TryOutOfQLineEdit()
+{
+    delete ui;
+}
+
+void TryOutOfQLineEdit::PrintOutput()
+{
+    qDebug() << "execute once!";
+}
