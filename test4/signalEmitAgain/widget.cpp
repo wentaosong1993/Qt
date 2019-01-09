f0f9b927bec4884769a92409a64294c4e4cae0cf
songwentao <614425032@qq.com>
2019-01-09 16:50:09 +0800

    一个槽函数可以循环触发执行

---
+++ test4/signalEmitAgain/widget.cpp
@@ -0,0 +1,25 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
+    connect(this,SIGNAL(clickSignal()),this,SLOT(buttonClick()));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::buttonClick()
+{
+    disconnect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
+    qDebug() << "execution";
+//    emit clickSignal();
+        connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
+}
