6284d669a89ac5c06b1fb6313e4653bf313baf1c
songwentao <614425032@qq.com>
2019-01-10 11:11:15 +0800

    Update widget.cpp

--- test4/signalEmitAgain/widget.cpp
+++ test4/signalEmitAgain/widget.cpp
@@ -1,14 +1,16 @@
 ﻿#include "widget.h"
 #include "ui_widget.h"
 #include <QDebug>
+#include <QFileDialog>
+#include <QStandardPaths>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+//    connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
     connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
-    connect(this,SIGNAL(clickSignal()),this,SLOT(buttonClick()));
 }
 
 Widget::~Widget()
@@ -18,8 +20,17 @@ Widget::~Widget()
 
 void Widget::buttonClick()
 {
-    disconnect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
-    qDebug() << "execution";
-//    emit clickSignal();
-        connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
+//    disconnect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
+//    qDebug() << "execution";
+////    emit clickSignal();
+//        connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(buttonClick()));
+    //临时文件路径
+//    QString fileName = QFileDialog::getOpenFileName(this,QStringLiteral("Open the dialog"),QStandardPaths::writableLocation(QStandardPaths::TempLocation),QStringLiteral("Images (*.png *.xpm *.jpg);;Text files (*.txt);;XML files (*.xml)"));
+//    if(!fileName.isEmpty())
+//    {
+//        qDebug() << fileName;
+//    }
+
+   QStringList standardPath = QStandardPaths::standardLocations(QStandardPaths::ApplicationsLocation);
+   qDebug() << standardPath;
 }
