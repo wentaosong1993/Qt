8b50a5ca4803213d6331a014eaae22516e19a774
songwentao <614425032@qq.com>
2019-01-20 22:29:15 +0800

    "Can't connect to MySQL server on '127.0.0.1' (10061) QMYSQL: Unable to connect"

---
+++ DataBase/dataBase1/widget.cpp
@@ -0,0 +1,31 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QMessageBox>
+#include <QSqlError>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    qDebug() << QSqlDatabase::drivers();
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("127.0.0.1");
+    db.setUserName("root");
+    db.setPassword("123456");
+    db.setDatabaseName("info");
+
+    if(!db.open())
+    {
+//        QMessageBox::warning(this,QStringLiteral("message"),db.lastError().text());
+      qDebug() << db.lastError().text();
+    }
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
