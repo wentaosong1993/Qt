435787945a4f2a425c526363402cf1ecbf7e824d
songwentao <614425032@qq.com>
2019-03-04 14:06:43 +0800

    const的常用语法

---
+++ Usage/const/widget.cpp
@@ -0,0 +1,42 @@
+﻿#include "widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+	FunctionConst func;
+	func.getValue();
+	func.getValue2();
+}
+
+Widget::~Widget()
+{
+}
+
+
+
+FunctionConst::FunctionConst() :value(100) {
+	// TODO Auto-generated constructor stub
+
+}
+
+FunctionConst::~FunctionConst() {
+	// TODO Auto-generated destructor stub
+}
+
+const int FunctionConst::getValue() {
+	return value;//返回值是 const, 使用指针时很有用.
+}
+
+int FunctionConst::getValue2() const {
+	//此函数不能修改class FunctionConst的成员函数 value
+	value = 15;//错误的, 因为函数后面加 const
+	return value;
+}
+
+/*
+1>widget.cpp(30): error C3490: 由于正在通过常量对象访问“value”，因此无法对其进行修改
+========== 生成: 成功 0 个，失败 1 个，最新 0 个，跳过 0 个 ==========
+*/
+
+
---
+++ Usage/const/widget.h
@@ -0,0 +1,35 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+};
+
+#endif // WIDGET_H
+
+
+#ifndef FUNCTIONCONST_H_
+#define FUNCTIONCONST_H_
+
+class FunctionConst {
+public:
+	int value;
+	FunctionConst();
+	virtual ~FunctionConst();
+	const int getValue();
+	int getValue2() const;
+};
+
+#endif /* FUNCTIONCONST_H_ */
+
+
+
+
+
