2347db8e49b45a675e9c2bceeef162cc238c9c87
songwentao <614425032@qq.com>
2019-03-08 13:26:47 +0800

    qml与C++交互

---
+++ CommunicationBetweenCPlusAndQt/Communication/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <QtGui/QGuiApplication>
+#include <QtQuick/QQuickView>
+#include <QtQml/QQmlEngine>
+#include "colormaker.h"
+
+int main(int argc,char *argv[])
+{
+    QGuiApplication app(argc,argv);
+    qmlRegisterType<colorMaker>("an.qt.colorMaker",1,0,"ColorMaker");
+
+    QQuickView viewer;
+    QObject::connect(viewer.engine(), SIGNAL(quit()), &app,SLOT(quit()));
+
+    viewer.setResizeMode(QQuickView::SizeRootObjectToView);
+    viewer.setSource(QUrl(":/main.qml"));
+    viewer.show();
+    return app.exec();
+}
