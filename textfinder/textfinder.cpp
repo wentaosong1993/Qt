b5d1155c890de35c8df9b166ceed03642639b592
songwentao <614425032@qq.com>
2019-03-21 15:11:26 +0800

    TextFinder
    字段查找

---
+++ textfinder/textfinder.cpp
@@ -0,0 +1,108 @@
+﻿#include <QtUiTools>
+#include <QtWidgets>
+#include "textfinder.h"
+
+
+TextFinder::TextFinder(QWidget *parent)
+    : QWidget(parent)
+{
+    QWidget *formWidget = loadUiFile();
+
+    ui_findButton = findChild<QPushButton*>("findButton");
+    ui_textEdit = findChild<QTextEdit*>("textEdit");
+    ui_lineEdit = findChild<QLineEdit*>("lineEdit");
+
+
+
+    QMetaObject::connectSlotsByName(this);
+
+
+
+    loadTextFile();
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(formWidget);
+    setLayout(layout);
+
+    setWindowTitle(tr("Text Finder"));
+    isFirstTime = true;
+}
+
+QWidget* TextFinder::loadUiFile()
+{
+    QUiLoader loader;
+
+    QFile file(":/forms/textfinder.ui");
+    file.open(QFile::ReadOnly);
+
+    QWidget *formWidget = loader.load(&file, this);
+    file.close();
+
+    return formWidget;
+}
+
+void TextFinder::loadTextFile()
+{
+    QFile inputFile(":/forms/input.txt");
+    inputFile.open(QIODevice::ReadOnly);
+    QTextStream in(&inputFile);
+    QString line = in.readAll();
+    inputFile.close();
+
+    ui_textEdit->append(line);
+    ui_textEdit->setUndoRedoEnabled(false);
+    ui_textEdit->setUndoRedoEnabled(true);
+}
+
+void TextFinder::on_findButton_clicked()
+{
+    QString searchString = ui_lineEdit->text();
+    QTextDocument *document = ui_textEdit->document();
+
+    bool found = false;
+
+    int i = 0;
+
+    if (isFirstTime == false)
+        document->undo();
+
+    if (searchString.isEmpty()) {
+        QMessageBox::information(this, tr("Empty Search Field"),
+                "The search field is empty. Please enter a word and click Find.");
+    } else {
+
+        QTextCursor highlightCursor(document);
+        QTextCursor cursor(document);
+
+        cursor.beginEditBlock();
+
+
+        QTextCharFormat plainFormat(highlightCursor.charFormat());
+        QTextCharFormat colorFormat = plainFormat;
+        colorFormat.setForeground(Qt::red);
+
+        while (!highlightCursor.isNull() && !highlightCursor.atEnd()) {
+            highlightCursor = document->find(searchString, highlightCursor, QTextDocument::FindWholeWords);
+
+            if (!highlightCursor.isNull()) {
+                found = true;
+                highlightCursor.movePosition(QTextCursor::WordRight,
+                                       QTextCursor::KeepAnchor);
+                highlightCursor.mergeCharFormat(colorFormat);
+                if(++i == 1)
+                {
+                    ui_textEdit->verticalScrollBar()->setValue(highlightCursor.position());
+                }
+            }
+        }
+
+        cursor.endEditBlock();
+
+        isFirstTime = false;
+
+        if (found == false) {
+            QMessageBox::information(this, tr("Word Not Found"),
+                "Sorry, the word cannot be found.");
+        }
+    }
+}
---
+++ textfinder/textfinder.h
@@ -0,0 +1,32 @@
+﻿#ifndef TEXTFINDER_H
+#define TEXTFINDER_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QPushButton;
+class QTextEdit;
+class QLineEdit;
+QT_END_NAMESPACE
+
+class TextFinder : public QWidget
+{
+    Q_OBJECT
+
+public:
+    TextFinder(QWidget *parent = 0);
+
+private slots:
+    void on_findButton_clicked();
+
+private:
+    QWidget* loadUiFile();
+    void loadTextFile();
+
+    QPushButton *ui_findButton;
+    QTextEdit *ui_textEdit;
+    QLineEdit *ui_lineEdit;
+    bool isFirstTime;
+};
+
+#endif
