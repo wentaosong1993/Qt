c5ff9cf8ff6a685a98529042ead56b0ca0fd6992
songwentao <614425032@qq.com>
2019-03-21 17:33:21 +0800

    创建插件隐并在应用程序中使用插件

---
+++ myplugin/plugin/regexpplugin.h
@@ -0,0 +1,19 @@
+﻿#ifndef REGEXPPLUGIN_H
+#define REGEXPPLUGIN_H
+
+#include <QObject>
+#include "regexpinterface.h"
+
+class RegExpPlugin : public QObject,RegExpInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qter.Example.myplugin.RegExpInterface"
+                      FILE "myplugin.json")
+
+    Q_INTERFACES(RegExpInterface)
+
+public:
+    QString regexp(const QString &message);
+};
+
+#endif // REGEXPPLUGIN_H
