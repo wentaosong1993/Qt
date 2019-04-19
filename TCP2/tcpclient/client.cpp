b341b0ad9dd0374edaab081f4f5a508245d95ba7
songwentao <614425032@qq.com>
2019-04-19 11:12:59 +0800

    Update client.cpp

--- TCP2/tcpclient/client.cpp
+++ TCP2/tcpclient/client.cpp
@@ -18,10 +18,8 @@ Client::Client(QWidget *parent) :
 
     // 当连接服务器成功时，发出connected()信号，开始传送文件
     connect(tcpClient, SIGNAL(connected()), this, SLOT(startTransfer()));
-    connect(tcpClient, SIGNAL(bytesWritten(qint64)),
-            this, SLOT(updateClientProgress(qint64)));
-    connect(tcpClient, SIGNAL(error(QAbstractSocket::SocketError)),
-            this, SLOT(displayError(QAbstractSocket::SocketError)));
+    connect(tcpClient, SIGNAL(bytesWritten(qint64)),this, SLOT(updateClientProgress(qint64)));
+    connect(tcpClient, SIGNAL(error(QAbstractSocket::SocketError)),this, SLOT(displayError(QAbstractSocket::SocketError)));
     ui->sendButton->setEnabled(false);
 }
 
@@ -33,7 +31,8 @@ Client::~Client()
 void Client::openFile()
 {
     fileName = QFileDialog::getOpenFileName(this);
-    if (!fileName.isEmpty()) {
+    if (!fileName.isEmpty())
+    {
         ui->sendButton->setEnabled(true);
         ui->clientStatusLabel->setText(QStringLiteral("打开文件 %1 成功！").arg(fileName));
     }
@@ -42,19 +41,18 @@ void Client::openFile()
 void Client::send()
 {
     ui->sendButton->setEnabled(false);
-
     // 初始化已发送字节为0
     bytesWritten = 0;
     ui->clientStatusLabel->setText(QStringLiteral("连接中…"));
-    tcpClient->connectToHost(ui->hostLineEdit->text(),
-                             ui->portLineEdit->text().toInt());
+    tcpClient->connectToHost(ui->hostLineEdit->text(),ui->portLineEdit->text().toInt());
 }
 
 
 void Client::startTransfer()
 {
     localFile = new QFile(fileName);
-    if (!localFile->open(QFile::ReadOnly)) {
+    if (!localFile->open(QFile::ReadOnly))
+    {
         qDebug() << "client: open file error!";
         return;
     }
@@ -64,18 +62,6 @@ void Client::startTransfer()
     QDataStream sendOut(&outBlock, QIODevice::WriteOnly);
     sendOut.setVersion(QDataStream::Qt_4_0);
     QString currentFileName = fileName.right(fileName.size() - fileName.lastIndexOf('/')-1);
-//    QString x = "crazy azimuths";
-//    QString y = "az";
-
-//    qDebug() << x.lastIndexOf(y);           // returns 6
-//    qDebug() << x.lastIndexOf(y, 6);        // returns 6
-//    qDebug() << x.lastIndexOf(y, 5);        // returns 2
-//    qDebug() << x.lastIndexOf(y, 1);        // returns -1
-
-//    qDebug() << fileName.size();
-//    qDebug() << fileName.lastIndexOf('/');
-//    qDebug() << fileName.right(fileName.size() - fileName.lastIndexOf('/'));
-//    qDebug() << "CurrentFileName:" << currentFileName;
 
     // 保留总大小信息空间、文件名大小信息空间，然后输入文件名
     sendOut << qint64(0) << qint64(0) << currentFileName;
@@ -86,7 +72,7 @@ void Client::startTransfer()
 
     // 返回outBolock的开始，用实际的大小信息代替两个qint64(0)空间
     sendOut << totalBytes << qint64((outBlock.size() - sizeof(qint64)*2));
-             //totalBytes << fileNameSize
+             //totalBytes << currentFileName
     // 发送完文件头结构后剩余数据的大小
     bytesToWrite = totalBytes - tcpClient->write(outBlock);
 
@@ -100,7 +86,8 @@ void Client::updateClientProgress(qint64 numBytes)
     bytesWritten += (int)numBytes;
 
     // 如果已经发送了数据
-    if (bytesToWrite > 0) {
+    if (bytesToWrite > 0)
+    {
         // 每次发送payloadSize大小的数据，这里设置为64KB，如果剩余的数据不足64KB，
         // 就发送剩余数据的大小
         outBlock = localFile->read(qMin(bytesToWrite, payloadSize));
@@ -110,14 +97,17 @@ void Client::updateClientProgress(qint64 numBytes)
 
         // 清空发送缓冲区
         outBlock.resize(0);
-    } else { // 如果没有发送任何数据，则关闭文件
+    }
+    else
+    { // 如果没有发送任何数据，则关闭文件
         localFile->close();
     }
     // 更新进度条
     ui->clientProgressBar->setMaximum(totalBytes);
     ui->clientProgressBar->setValue(bytesWritten);
     // 如果发送完毕
-    if(bytesWritten == totalBytes) {
+    if(bytesWritten == totalBytes)
+    {
         ui->clientStatusLabel->setText(QStringLiteral("传送文件 %1 成功").arg(fileName));
         localFile->close();
         tcpClient->close();
@@ -140,7 +130,6 @@ void Client::on_openButton_clicked()
     ui->clientProgressBar->reset();
     ui->clientStatusLabel->setText(QStringLiteral("状态：等待打开文件！"));
     openFile();
-
 }
 
 // 发送按钮
