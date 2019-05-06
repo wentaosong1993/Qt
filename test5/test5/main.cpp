6f98de1b62b021a87b334ff9da84e34250fa52c7
songwentao <614425032@qq.com>
2019-05-06 15:07:08 +0800

    C++中delete的作用
    delete对象释放操作符，触发析构函数，释放内存

---
+++ test5/test5/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+
+
+
+class A
+{
+public :
+    A(){qDebug() << "constructor";}
+    ~A()
+    {
+        qDebug() << "destructor";
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    A *b = new A;
+    delete b; //delete调用析构函数，释放对象占用的内存；
+    b = nullptr;
+    return a.exec();
+}
