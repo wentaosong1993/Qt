18f3e5db116d83d16dad39c4a3b67e0dc701944c
songwentao <614425032@qq.com>
2019-05-06 16:52:45 +0800

    拖放操作--通过修饰符控制复制、移动、链接等操作没有成功。目前通过修饰符改变鼠标形状

--- imagedragdrop/mainwindow.h
+++ imagedragdrop/mainwindow.h
@@ -20,8 +20,8 @@ private:
 
 protected:
     void mousePressEvent(QMouseEvent *event);      // 鼠标按下事件
+	void mouseMoveEvent(QMouseEvent *event);
     void dragEnterEvent(QDragEnterEvent *event);   // 拖动进入事件
-    void dragMoveEvent(QDragMoveEvent *event);     // 拖动事件
     void dropEvent(QDropEvent *event);             // 放下事件
 
 };
