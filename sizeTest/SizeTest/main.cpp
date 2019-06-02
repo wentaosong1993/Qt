19ecc29b9df121e2df60596f9468b08654735308
songwentao <614425032@qq.com>
2019-06-02 14:57:22 +0800

    Update main.cpp

--- sizeTest/SizeTest/main.cpp
+++ sizeTest/SizeTest/main.cpp
@@ -18,6 +18,11 @@ int main(int argc, char *argv[])
     QRect geometry = w.geometry();
     QRect frame = w.frameGeometry();
     qDebug() << "geometry:" << geometry << "frame:" << frame;
+
+    qDebug() << "pos:" << w.pos() << endl << "rect:" << w.rect()
+             << endl << "size:" << w.size() << endl << "width:"
+             << w.width() << endl << "height:" << w.height();
+    //宽、高、大小不包括窗口的外框和菜单栏
     return a.exec();
 }
 
@@ -25,4 +30,9 @@ int main(int argc, char *argv[])
  * x: 200
    y: 100
    geometry: QRect(208,130 400x300) frame: QRect(200,100 416x338)
+   pos: QPoint(200,100)
+   rect: QRect(0,0 400x300)
+   size: QSize(400, 300)
+   width: 400
+   height: 300
 */
