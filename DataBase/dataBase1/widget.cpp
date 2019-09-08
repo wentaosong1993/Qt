4e714e97909ae051f9b32c8a1f3b99e6d3e08503
songwentao <614425032@qq.com>
2019-09-08 21:24:53 +0800

    解决8个月以前滞留的问题
    解决无法连接MySQL的问题。

--- DataBase/dataBase1/widget.cpp
+++ DataBase/dataBase1/widget.cpp
@@ -12,15 +12,22 @@ Widget::Widget(QWidget *parent) :
     ui->setupUi(this);
     qDebug() << QSqlDatabase::drivers();
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+//        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setHostName("127.0.0.1");
     db.setUserName("root");
-    db.setPassword("123456");
+    db.setPassword("songwentao");
+    //setdatabasename（）必须是已经存在的数据库
     db.setDatabaseName("info");
+//    db.setPort(3306);//3307
 
     if(!db.open())
     {
       QMessageBox::warning(this,QStringLiteral("message"),db.lastError().text());
       qDebug() << db.lastError().text();
+    }
+    else
+    {
+        QMessageBox::information(this,QStringLiteral("message"),"Success!");
     }
 
 }
